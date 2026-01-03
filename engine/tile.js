53ab16552e77daa782bd4d3bd7f52dd01296c1bd
KC-AiOS <kcaios.system@gmail.com>
2026-01-03 22:19:52 +0800

    Create tile.js

---
+++ engine/tile.js
@@ -0,0 +1,7 @@
+//------------------------------------------------------
+// tile.js - Basic Tile Size Constant
+//------------------------------------------------------
+
+console.log("tile.js loaded");
+
+const TILE = 10;  // 10 pixels per tile (32x24 fits 320x240)
